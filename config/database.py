a2033a49dc6d538ae84bec6bc5bb527b3116d4b0
chayan007 <sonicxxx7@gmail.com>
2021-02-01 16:41:14 +0530

    mysqlalchemy db session and base

--- config/database.py
+++ config/database.py
@@ -1,9 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
 from config.server import server_environment
 
 from constants.environment import Environment
 
-if server_environment == Environment.LOCAL:
-    pass
 
-if server_environment == Environment.PRODUCTION:
-    pass
+class GlobalDBHelper:
+
+    base = None
+    engine = None
+    session = None
+
+    def __init__(self):
+        if server_environment == Environment.LOCAL:
+            self.__setup_local_database()
+
+        if server_environment == Environment.PRODUCTION:
+            self.__setup_production_database()
+
+    def __setup_local_database(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        self.base = declarative_base()
+        self.session = Session(bind=self.engine)
+
+    def __setup_production_database(self):
+        pass
