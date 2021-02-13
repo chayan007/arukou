c5128ac9ab5ff66570b5fd2872c94002f31d2eda
chayan007 <sonicxxx7@gmail.com>
2021-02-14 01:48:14 +0530

    Adds authenticator class

---
+++ modules/authentication/authenticator.py
@@ -0,0 +1,17 @@
+class Authenticator:
+
+    def register(self):
+        """Register an user."""
+        pass
+
+    def login(self):
+        """Login an user."""
+        pass
+
+    def verify(self):
+        """Verify an user."""
+        pass
+
+    def reset_password(self):
+        """Reset password for an user."""
+        pass
--- modules/authentication/user.py
+++ modules/authentication/user.py
@@ -1,2 +1,17 @@
-class UserAuthenticator:
-    pass
+class User:
+
+    def update(self):
+        """Update user details."""
+        pass
+
+    def deactivate(self):
+        """Deactivate an user."""
+        pass
+
+    def report(self):
+        """Report an user."""
+        pass
+
+    def block(self):
+        """Block an user."""
+        pass
